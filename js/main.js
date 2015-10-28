23141cfd2dd84c4b461fdc6707a4c9718311dc0c
Johannes Hilden <hildenjohannes@gmail.com>
2015-10-28 13:11:51 +0300

    add public folder

--- js/main.js
+++ js/main.js
@@ -2,13 +2,27 @@ import React       from 'react'
 import ReactDOM    from 'react-dom'
 import Grid        from '../modules/Grid'
 
+const data = [
+  {
+    name : 'Palak Paneer'
+  },
+  {
+    name : 'Kadai Veg'
+  }
+]
+
 class App extends React.Component {
   constructor(props) {
     super(props)
   }
   render() {
     return (
-      <Grid />
+      <div>
+        hello
+        <Grid 
+          columns = {['name']}
+          data    = {data} />
+      </div>
     )
   }
 }
